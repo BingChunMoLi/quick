8991b0e88f5f9d0b824ef32a768782dd7db396fb
BingChunMoLi <44092516+BingChunMoLi@users.noreply.github.com>
2023-06-03 21:01:57 +0800

    feat(1.0.8-bate): test aop

--- moli-quick-aop-boot-starter/src/main/java/com/bingchunmoli/autoconfigure/aop/LogAutoConfiguration.java
+++ moli-quick-aop-boot-starter/src/main/java/com/bingchunmoli/autoconfigure/aop/LogAutoConfiguration.java
@@ -3,20 +3,16 @@ package com.bingchunmoli.autoconfigure.aop;
 import com.bingchunmoli.aspect.LogAspect;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author MoLi
  */
-@Configuration
+
+@Import(LogAspect.class)
 @ConditionalOnClass(LogAutoConfiguration.class)
 @ConditionalOnMissingBean(LogAutoConfiguration.class)
 public class LogAutoConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean(LogAspect.class)
-    public LogAspect logAspect(){
-        return new LogAspect();
-    }
+
 }
--- moli-quick-spring-boot-starter-common-test/src/main/java/com/bingchunmoli/quick/TestApplication.java
+++ moli-quick-spring-boot-starter-common-test/src/main/java/com/bingchunmoli/quick/TestApplication.java
@@ -14,8 +14,4 @@ public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
-
-
-
-
 }
