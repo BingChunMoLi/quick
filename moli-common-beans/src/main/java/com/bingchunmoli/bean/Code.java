7c24c670883f5392ed218430c0c14f3c44955fb3
BingChunMoLi <44092516+BingChunMoLi@users.noreply.github.com>
2023-12-25 23:54:08 +0800

    refactor(1.0.9-bate): Code接口应该是可序列化的

--- moli-common-beans/src/main/java/com/bingchunmoli/bean/Code.java
+++ moli-common-beans/src/main/java/com/bingchunmoli/bean/Code.java
@@ -1,10 +1,12 @@
 package com.bingchunmoli.bean;
 
+import java.io.Serializable;
+
 /**
  * 业务Code接口
  * @author MoLi
  */
-public interface Code {
+public interface Code extends Serializable {
     String getCode();
     String getMsg();
 }
